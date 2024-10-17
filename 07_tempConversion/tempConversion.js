4b5a40d16d04aa9f0dd1c66d3c1d8fed2ae1e9fd
Shadya Jolliet <shadya.jolliet27@gmail.com>
2024-10-17 16:23:10 +0700

    Complete exo 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const roundToOneDecimal = function(num) {
+  return parseFloat(num.toFixed(1));
+}
+
+const convertToCelsius = function(fahrenheit) {
+  return roundToOneDecimal((fahrenheit - 32) * (5 / 9));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return roundToOneDecimal(celsius * (9 / 5) + 32);
 };
 
 // Do not edit below this line
