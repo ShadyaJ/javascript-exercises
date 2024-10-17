ab08b01fb3620699102439ea5a289d9ab66e7003
Shadya Jolliet <shadya.jolliet27@gmail.com>
2024-10-17 15:13:52 +0700

    Complete exo 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Leap years are divisible by 4. If they are also divisible by 100, they are not leap years except if divisible by 400.
+    return (year % 400 === 0) || (year % 100 !== 0 && year % 4 === 0);
 };
 
 // Do not edit below this line
