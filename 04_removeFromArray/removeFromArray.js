a262af8e9d5f05975024c45b566e8a8650cd1801
Shadya Jolliet <shadya.jolliet27@gmail.com>
2024-10-17 14:25:44 +0700

    Complete exo 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    // My solution :
+    for(const elementToRemove of elements){
+        array = array.filter(element => element !== elementToRemove);
+    }
+
+    return array;
+
+    // Refractored solution :
+    // return array.filter(element => !elements.includes(element));
 };
 
 // Do not edit below this line
