7038d422fe12f72bbe1afac2780cb33147f98809
Shadya Jolliet <shadya.jolliet27@gmail.com>
2024-10-31 19:02:25 +0800

    Finish exo 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const sortedArr = arr.sort((a, b) => {
+        const currentYear = new Date().getFullYear();
+        const personAAge = a.yearOfDeath ? a.yearOfDeath - a.yearOfBirth : currentYear - a.yearOfBirth;
+        const personBAge = b.yearOfDeath ? b.yearOfDeath - b.yearOfBirth : currentYear - b.yearOfBirth;
 
+        return personBAge - personAAge;
+    });
+    return sortedArr[0];
 };
 
 // Do not edit below this line
