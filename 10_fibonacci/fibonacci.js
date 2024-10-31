a41bab96af0b0561528d1b5fd75499fe94e09699
Shadya Jolliet <shadya.jolliet27@gmail.com>
2024-10-31 15:37:51 +0800

    Finish exo 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0) { return 'OOPS' };
+    if(num == 0) { return 0 };
 
+    const sequence = [1, 1];
+
+    while(sequence.length < num) {
+        sequence.push(sequence[sequence.length - 2] + sequence[sequence.length - 1]);
+    }
+
+    return sequence[num - 1];
 };
 
 // Do not edit below this line
